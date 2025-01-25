48eb46d25ec3370ffc8aa7881c98aa7390d1fd20
Stoyan <podlobg@gmail.com>
2025-01-25 19:43:29 +0200

    Revert "switching places of class properties"
    This reverts commit 8d6f696eeb2b72ecbdcc46ce49982f7c85a365e1.

--- Version Control with Git/GitBasics/GitBasics/Program.cs
+++ Version Control with Git/GitBasics/GitBasics/Program.cs
@@ -6,22 +6,22 @@
         {
             var allPersons = new List<Person>();
 
-            var newPerson = new Person("Hi", "Stoyan", 25);
+            var newPerson = new Person("Stoyan", 25, "hi");
 
             allPersons.Add(newPerson);
-            allPersons.Add(new Person("Hi"));
+            allPersons.Add(new Person("Richi"));
 
             foreach (var peron in allPersons)
             {
-                Console.WriteLine(peron.Speak);
+                Console.WriteLine(peron.Name);
 
-                if (peron.Age == 0 || peron.Name is null)
+                if (peron.Age == 0 || peron.Speak is null)
                 {
                     continue;
                 }
 
-                Console.WriteLine(peron.Name);
                 Console.WriteLine(peron.Age);
+                Console.WriteLine(peron.Speak);
             }
 
         }
