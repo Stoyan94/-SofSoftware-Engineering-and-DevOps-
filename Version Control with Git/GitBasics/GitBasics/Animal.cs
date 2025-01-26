b647be3cf63f66c385e8a34b366599464dc95e12
Stoyan <podlobg@gmail.com>
2025-01-26 14:03:59 +0200

    new class Animal

---
+++ Version Control with Git/GitBasics/GitBasics/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace GitBasics
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string sound)
+        {
+            Name = name;
+            Age = age;
+            Sound = sound;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sound { get; set; }
+    }
+}
