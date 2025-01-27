1422ef421621f405db9aff9bb4fb53af5d6c98f2
Stoyan <podlobg@gmail.com>
2025-01-27 21:36:12 +0200

    first-cherry

--- Exercise Version Control with Git/GtiEx/GtiEx/Program.cs
+++ Exercise Version Control with Git/GtiEx/GtiEx/Program.cs
@@ -9,6 +9,7 @@
             var fordCar = new Car("Ford");
 
             var nissanCar = new Car("Nissan", "GTR", "John Doe", "1995");
+            var toyotaCar = new Car("Toyota", "Supra", "John Doe", "1995");
 
             cars.Add(fordCar);
             cars.Add(nissanCar);
@@ -17,6 +18,7 @@
 
             var fordTruck = new Truck("Ford", "F-150", "Jane Doe", "1992");
             var nissanTruck = new Truck("Nissan", "Titan", "John Doe", "1999");
+            var toyotaTruck = new Truck("Toyota", "Tundra", "John Doe", "2000");
 
             trucks.Add(fordTruck);
             trucks.Add(nissanTruck);
